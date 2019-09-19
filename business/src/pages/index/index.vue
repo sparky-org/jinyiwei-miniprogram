3c1780f083b4c46edf8a68ca0c9da5cc2e87fb39
zzhnokia <zzhnokia@163.com>
2019-09-19 11:47:54 +0800

    modify

--- business/src/pages/index/index.vue
+++ business/src/pages/index/index.vue
@@ -21,7 +21,7 @@ export default {
     let role = 'admin'
     setTimeout(() => {
       if(role === 'admin'){
-        // 保留当前页面，跳转到应用内的某个页面。但是不能跳到 tabbar 页面  返回是 wx.navigateBack
+        // 保留当前页面，跳转到应用内的某个页面。但是不能跳到 tabbar 页面  返回是 wx.navigateBack({ })
         // wx.navigateTo({
         //   url: '/pages/adminIndex/main'
         // })
