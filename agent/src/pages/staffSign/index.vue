8afbc7766bd84d0191774906ec01ef70babf8099
zzhnokia <zzhnokia@163.com>
2019-10-18 07:44:01 +0800

    modify

--- agent/src/pages/staffSign/index.vue
+++ agent/src/pages/staffSign/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="staff_sign">
 
-    <div class="map">
-
+    <div class="map" v-if="latitude && longitude">
+      <map id="map" :markers="markers" :longitude="longitude" :latitude="latitude" scale="14" show-location style="width: 100%; height: 300px;"></map>
     </div>
 
     <div class="weui-form-preview">
@@ -26,32 +26,54 @@
 <script>
 import amapFile from "../../utils/amap-wx";
 import { get } from "../../utils";
-import { mapState, mapMutations } from "vuex";
+let img = require('../../images/marker.png')
+// import { mapState, mapMutations } from "vuex";
 export default {
   onShow() {
   },
   computed: {
-    ...mapState(["cityName"])
+    // ...mapState(["cityName"])
   },
   mounted() {
-
-    this.getData();
+    wx.getLocation({
+      type: 'gcj02',
+      success: (res) => {
+        console.log("当前坐标信息：", res)
+        this.longitude = res.longitude
+        this.latitude = res.latitude
+        this.markers[0].longitude = res.longitude
+        this.markers[0].latitude = res.latitude
+        var distance = this.distance(res.latitude, res.longitude,39.918034,116.415192);
+        console.log("当前位置距离北京故宫：", distance, "千米")
+      }
+    })
+    // this.getData();
   },
   data() {
     return {
-      date: '2019-08-11'
+      markers: [{
+        iconPath: img,
+        // width: 50,
+        // height: 50,
+        id: 0,
+        latitude: '',
+        longitude: '',
+      }],
+      latitude: '',
+      longitude: ''
+      // date: '2019-08-11'
     };
   },
   components: {},
   methods: {
-    bindDateChange(e) {
-      console.log('选中的日期为：' + e.mp.detail.value);
-      this.date = e.mp.detail.value
-    },
-    ...mapMutations(["update"]),
+    // bindDateChange(e) {
+    //   console.log('选中的日期为：' + e.mp.detail.value);
+    //   this.date = e.mp.detail.value
+    // },
+    // ...mapMutations(["update"]),
 
     async getData() {
-      const data = await get("/index/index");
+      // const data = await get("/index/index");
       // this.banner = data.banner;
       // this.channel = data.channel;
       // this.brandList = data.brandList;
@@ -60,14 +82,37 @@ export default {
       // this.topicList = data.topicList;
       // this.newCategoryList = data.newCategoryList;
     },
-    goodsDetail(id) {
-      wx.navigateTo({
-        url: "/pages/goods/main?id=" + id
-      });
-    }
+    // goodsDetail(id) {
+    //   wx.navigateTo({
+    //     url: "/pages/goods/main?id=" + id
+    //   });
+    // }
+    /**
+       * @creator 新猿意码
+       * @data 2019/01/17
+       * @desc 由经纬度计算两点之间的距离，la为latitude缩写，lo为longitude
+       * @param la1 第一个坐标点的纬度
+       * @param lo1 第一个坐标点的经度
+       * @param la2 第二个坐标点的纬度
+       * @param lo2 第二个坐标点的经度
+       * @return (int)s   返回距离(单位千米或公里)
+       * @tips 注意经度和纬度参数别传反了，一般经度为0~180、纬度为0~90
+       * 具体算法不做解释，有兴趣可以了解一下球面两点之间最短距离的计算方式
+       */
+      distance: function (la1, lo1, la2, lo2) {
+        var La1 = la1 * Math.PI / 180.0;
+        var La2 = la2 * Math.PI / 180.0;
+        var La3 = La1 - La2;
+        var Lb3 = lo1 * Math.PI / 180.0 - lo2 * Math.PI / 180.0;
+        var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(La3 / 2), 2) + Math.cos(La1) * Math.cos(La2) * Math.pow(Math.sin(Lb3 / 2), 2)));
+        s = s * 6378.137;
+        s = Math.round(s * 10000) / 10000;
+        s = s.toFixed(2);
+        return s;
+      }
   },
   created() {
-    console.log(89128921982189)
+    // console.log(89128921982189)
   }
 };
 </script>
