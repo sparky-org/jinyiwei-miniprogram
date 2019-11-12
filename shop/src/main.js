38b0a809f43411b5a775bed7128348668d112f84
zzhnokia <zzhnokia@163.com>
2019-11-12 10:45:16 +0800

    commit

--- shop/src/main.js
+++ shop/src/main.js
@@ -41,7 +41,7 @@ export default {
       "pages/task/main",
       "pages/customer/main",
       "pages/index/main",
-      "^pages/my-score-action/main",
+      "^pages/my-score-action-detail/main",
 
 
 
---
+++ shop/src/pages/my-score-action-detail/index.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="page">
+
+    <!-- <div class="weui-toptips weui-toptips_warn" v-if="errorTips">错误提示</div> -->
+
+    <div class="weui-cells weui-cells_after-title">
+      <div class="weui-cell weui-cell_input">
+        <div class="weui-cell__hd">
+          <div class="weui-label">标题</div>
+        </div>
+        <div class="weui-cell__bd">
+          <input class="weui-input" placeholder="请输入标题" />
+        </div>
+      </div>
+
+      <div class="weui-cell weui-cell_input">
+        <div class="weui-cell__hd">
+          <div class="weui-label">内容</div>
+        </div>
+        <div class="weui-cell__bd">
+          <input class="weui-input" placeholder="请输入内容" />
+        </div>
+      </div>
+
+    </div>
+
+    <div class="page__bd">
+      <div class="weui-cells" style="margin-top: 0; border-top: 0 none; margin-top: -1rpx;">
+        <div class="weui-cell">
+          <div class="weui-cell__bd">
+            <div class="weui-uploader">
+              <div class="weui-uploader__hd">
+                <div class="weui-uploader__title">附加照片</div>
+                <div class="weui-uploader__info">{{files.length}}/2</div>
+              </div>
+              <div class="weui-uploader__bd">
+                <div class="weui-uploader__files" id="uploaderFiles">
+                  <div v-for="(item ,index) in files" :key="index">
+                    <div class="weui-uploader__file">
+                      <image class="weui-uploader__img" :src="item" mode="aspectFill" @click="predivImage" :id="item" />
+                      <div class="delete-icon" @click="deleteImg" :id="item"></div>
+                    </div>
+                  </div>
+                  <div class="weui-uploader__file">
+                    <image class="weui-uploader__img" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4133574179,304311957&fm=26&gp=0.jpg" mode="aspectFill" />
+                  </div>
+                  <div class="weui-uploader__file">
+                    <image class="weui-uploader__img" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4133574179,304311957&fm=26&gp=0.jpg" mode="aspectFill" />
+                  </div>
+                  <div class="weui-uploader__file">
+                    <image class="weui-uploader__img" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4133574179,304311957&fm=26&gp=0.jpg" mode="aspectFill" />
+                  </div>
+                 <!-- <div class="weui-uploader__file weui-uploader__file_status">
+                    <image class="weui-uploader__img" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4133574179,304311957&fm=26&gp=0.jpg" mode="aspectFill" />
+                    <div class="weui-uploader__file-content">
+                      <icon type="warn" size="23" color="#F43530"></icon>
+                    </div>
+                  </div>
+                  <div class="weui-uploader__file weui-uploader__file_status">
+                    <image class="weui-uploader__img" src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4133574179,304311957&fm=26&gp=0.jpg" mode="aspectFill" />
+                    <div class="weui-uploader__file-content">50%</div>
+                  </div> -->
+                </div>
+                <!-- <div class="weui-uploader__input-box">
+                  <div class="weui-uploader__input" @click="chooseImage"></div>
+                </div> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="weui-cells weui-cells_after-title no-t">
+      <div class="weui-cell weui-cell_input">
+        <div class="weui-cell__hd">
+          <div class="weui-label">被奖扣员工</div>
+        </div>
+        <div class="weui-cell__bd">
+          <input class="weui-input" placeholder="请输入奖罚积分" />
+        </div>
+      </div>
+      <div class="weui-cell weui-cell_input">
+        <div class="weui-cell__hd">
+          <div class="weui-label">奖扣人</div>
+        </div>
+        <div class="weui-cell__bd">
+          <input class="weui-input" placeholder="请输入奖罚积分" />
+        </div>
+      </div>
+      <div class="weui-cell weui-cell_input">
+        <div class="weui-cell__hd">
+          <div class="weui-label">奖扣时间</div>
+        </div>
+        <div class="weui-cell__bd">
+          <input class="weui-input" placeholder="请输入奖罚积分" />
+        </div>
+      </div>
+      <div class="weui-cell weui-cell_input">
+        <div class="weui-cell__hd">
+          <div class="weui-label">奖罚积分</div>
+        </div>
+        <div class="weui-cell__bd">
+          <input class="weui-input" placeholder="请输入奖罚积分" />
+        </div>
+      </div>
+    </div>
+
+
+    <!-- <div class="operate-btn">
+      <button class="weui-btn" type="primary">提 交</button>
+    </div> -->
+
+  </div>
+</template>
+
+<script>
+import amapFile from "../../utils/amap-wx";
+import { get } from "../../utils";
+// import { mapState, mapMutations } from "vuex";
+export default {
+  onShow() {
+
+  },
+  components: {
+
+  },
+  data() {
+    return {
+      // role: '',
+      errorTips: false,
+      files: []
+    };
+  },
+
+  mounted() {
+    // this.role = this.$store.state.userInfo.role
+    // console.info('v-show="$store.state.userInfo.role',this.$store.state.userInfo.role);
+    // this.getData();
+  },
+  computed: {
+
+  },
+  methods: {
+    
+    
+   
+
+    // handleAreaSelect(){
+    //   wx.showActionSheet({
+    //     itemList: enumArea,
+    //     success: (res) => {
+    //       console.log(res.tapIndex)
+    //       this.area = enumArea[res.tapIndex]
+    //     }
+    //   });
+    // },
+    // handleScorSelect(){
+    //   wx.showActionSheet({
+    //     itemList: scoreArea,
+    //     success: (res) => {
+    //       console.log(res.tapIndex)
+    //       this.score = scoreArea[res.tapIndex]
+    //     }
+    //   });
+    // }
+
+    // async getData() {
+    //   const data = await get("/index/index");
+    //   this.banner = data.banner;
+    //   this.channel = data.channel;
+    //   this.brandList = data.brandList;
+    //   this.newGoods = data.newGoods;
+    //   this.hotGoods = data.hotGoods;
+    //   this.topicList = data.topicList;
+    //   this.newCategoryList = data.newCategoryList;
+    // },
+    // goodsDetail(id) {
+    //   wx.navigateTo({
+    //     url: "/pages/goods/main?id=" + id
+    //   });
+    // }
+  },
+  created() {
+
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+  .weui-uploader__file {
+    position: relative;
+  }
+  .weui-uploader__bd {
+    overflow: visible;
+  }
+  .delete-icon {
+    position: absolute;
+    width: 40rpx;
+    height: 40rpx;
+    background: #f43530;
+    right: 0;
+    top: -20rpx;
+    border-radius: 40rpx;
+    z-index: 5;
+  }
+  .delete-icon::before {
+    content: '';
+    width: 26rpx;
+    height: 4rpx;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+  }
+  @import "./style.scss";
+</style>
