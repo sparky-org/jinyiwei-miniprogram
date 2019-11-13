3709230c09661ce90ad0afdd27207be8e3db55bc
zzhnokia <zzhnokia@163.com>
2019-11-13 10:19:39 +0800

    add

---
+++ shop/src/pages/my-appointment/index.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="page">
+
+    <Calendar
+      :months="months"
+      :value="value"
+      @next="next"
+      @prev="prev"
+      :events="events"
+      lunar
+      clean
+      @select="select"
+      ref="calendar"
+      @selectMonth="selectMonth"
+      @selectYear="selectYear"
+      :arrowLeft="arrowLeft"
+      :tileContent="tileContent"
+      :almanacs="almanacs"
+    />
+    <!-- <Calendar
+      :months="months"
+      :value="value"
+      @next="next"
+      @prev="prev"
+      :events="events"
+      lunar
+      clean
+      @select="select"
+      ref="calendar"
+      @selectMonth="selectMonth"
+      @selectYear="selectYear"
+      :arrowLeft="arrowLeft"
+      :tileContent="tileContent"
+      :almanacs="almanacs"
+    /> -->
+    <!-- <button @click="setToday">返回今日</button>
+    <button @click="dateInfo">日期信息</button>
+    <button @click="renderer">重新渲染年月日期</button> -->
+
+
+    <div class="weui-panel__bd" style="margin-top: -20rpx; border-top: 1rpx solid #ccc;">
+      <div class="weui-flex">
+        <div class="weui-flex__item">
+          <div class="placeholder">
+            <dl class="ph-dl">
+              <dt>1</dt>
+              <dd>迟到</dd>
+            </dl>
+          </div>
+        </div>
+        <div class="weui-flex__item">
+          <div class="placeholder">
+            <dl class="ph-dl">
+              <dt>20</dt>
+              <dd>早退</dd>
+            </dl>
+          </div>
+        </div>
+        <div class="weui-flex__item">
+          <div class="placeholder">
+            <dl class="ph-dl">
+              <dt>20</dt>
+              <dd>旷工</dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="weui-form-preview">
+      <!-- <div class="weui-form-preview__hd">
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">付款金额</div>
+          <div class="weui-form-preview__value_in-hd">¥2400.00</div>
+        </div>
+      </div> -->
+      <div class="weui-form-preview__bd">
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">上班时间</div>
+          <div class="weui-form-preview__value" style="text-align: left;">10:00 - 19:30</div>
+        </div>
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">签到</div>
+          <div class="weui-form-preview__value" style="text-align: left;">09:30</div>
+        </div>
+        <div class="weui-form-preview__item">
+          <div class="weui-form-preview__label">签退</div>
+          <div class="weui-form-preview__value" style="text-align: left;">20:12</div>
+        </div>
+      </div>
+      <!-- <div class="weui-form-preview__ft">
+        <navigator url="" class="weui-form-preview__btn weui-form-preview__btn_primary" hover-class="weui-form-preview__btn_active">操作</navigator>
+      </div> -->
+    </div>
+
+    <div class="operate-btn">
+      <button class="weui-btn" type="primary" @click="handleSign">打 卡</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import amapFile from "../../utils/amap-wx";
+import { get } from "../../utils";
+// import { mapState, mapMutations } from "vuex";
+
+import Calendar from 'mpvue-calendar'
+import 'mpvue-calendar/src/style.css'
+
+export default {
+  onShow() {
+
+  },
+  components: {
+    Calendar
+  },
+  data() {
+    return {
+      // months: ['一月', '二月', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      // disabledArray: ['2018-6-27','2018-6-25'],
+      // value: [2018,6,7],
+      // begin:[2016,1,1],
+      // end:[2020,1,1],
+      events: {}, //events为自定义备注，例如备注2018年6月21日为{'2018-6-21': '今日备注', '2018-6-22':'明日备注'}，在clean模式下备注为圆点，lunar农历模式下备注会替代农历优先展示
+      // almanacs: {'9-3': '抗战胜利日', '11-17': '学生日'}, // 自定义节日
+      tileContent: [], // tileContent参数可以为具体某日定义一个class名，还可以插入一段文本内容。如[{date: '2018-9-20', className: 'holiday', content: '休'}]可以设置2018-9-20这天的class名为holiday，并且生成一个文本内容为 休 的dom节点
+    };
+  },
+
+  mounted() {
+    setTimeout(()=>{
+      // this.events = {
+      //   '2019-11-15':'预约',
+      //   '2019-11-18':'预约'
+      // }
+      this.tileContent.push({date: '2019-11-12', className: 'holiday', content: '预约'},{date: '2019-11-19', className: 'errorDate', content: '异常'}, {date: '2019-11-13', className: 'holiday', content: '预约'})
+    },3000)
+    // this.calendar.jump(2021, 10);
+    // this.id = this.$root.$mp.query.id;
+    // console.info(this.id)
+    // this.role = this.$store.state.userInfo.role
+    // console.info('v-show="$store.state.userInfo.role',this.$store.state.userInfo.role);
+    // this.getData();
+  },
+  computed: {
+
+  },
+  methods: {
+
+    prev(year, month, weekIndex) {
+      console.log(year, month, weekIndex)
+    },
+    next(year, month, weekIndex) {
+      console.log(year, month, weekIndex)
+    },
+    selectYear(year) {
+      console.log(year)
+    },
+    selectMonth(month, year) {
+      console.log(year, month)
+    },
+    setToday() {
+      this.$refs.calendar.setToday()
+    },
+    dateInfo() {
+      const info = this.$refs.calendar.dateInfo(2018, 8, 23)
+      console.log(info);
+    },
+    renderer() {
+      this.$refs.calendar.renderer(2018, 8); //渲染2018年8月份
+    },
+    select(val, val2) {
+      console.log(val)
+      console.log(val2)
+    },
+
+    // async getData() {
+    //   const data = await get("/index/index");
+    //   this.banner = data.banner;
+    //   this.channel = data.channel;
+    //   this.brandList = data.brandList;
+    //   this.newGoods = data.newGoods;
+    //   this.hotGoods = data.hotGoods;
+    //   this.topicList = data.topicList;
+    //   this.newCategoryList = data.newCategoryList;
+    // },
+    handleSign() {
+      wx.navigateTo({
+        url: "/pages/my-sign/main?"
+      });
+    }
+  },
+  created() {
+
+  }
+};
+</script>
+
+<style lang='scss'>
+  @import "./style.scss";
+
+</style>
