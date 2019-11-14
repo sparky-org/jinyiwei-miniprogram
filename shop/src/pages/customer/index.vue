123ac770045e6c149e2153f4da361f3a015490f1
zzhnokia <zzhnokia@163.com>
2019-11-14 14:55:22 +0800

    add

--- shop/src/pages/customer/index.vue
+++ shop/src/pages/customer/index.vue
@@ -24,8 +24,8 @@
 
 
     <div class="weui-cells__title">活跃客户曲线</div>
-    <div class="echart-area" style="line-height: 150px; text-align: center; color: #ccc;">
-      这个是一个图表
+    <div class="echart-area">
+      <mpvue-echarts :echarts="echarts" :onInit="onInit" />
     </div>
 
 
@@ -93,11 +93,64 @@
 <script>
 import amapFile from "../../utils/amap-wx";
 import { get } from "../../utils";
+
+import * as echarts from 'echarts/dist/echarts.simple.min'
+import mpvueEcharts from 'mpvue-echarts'
+
+function initChart (canvas, width, height) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  })
+  canvas.setChart(chart)
+  var option = {
+    backgroundColor: '#fff',
+    color: ['#37A2DA', '#67E0E3'],
+
+    legend: {
+      data: ['A', 'B']
+    },
+    grid: {
+      containLabel: true
+    },
+    xAxis: {
+      type: 'category',
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
+    },
+    series: [{
+      name: 'A',
+      type: 'line',
+      smooth: true,
+      data: [18, 36, 65, 30, 78, 40, 33]
+    }, {
+      name: 'B',
+      type: 'line',
+      smooth: true,
+      data: [12, 50, 51, 35, 70, 30, 20]
+    }]
+  }
+  chart.setOption(option)
+  return chart
+};
+
+
 export default {
   onShow() {
   },
   computed: {
 
+  },
+  components: {
+    mpvueEcharts
   },
   mounted() {
     // this.role = this.$store.state.userInfo.role
@@ -107,9 +160,10 @@ export default {
   data() {
     return {
       // role: ''
+      echarts,
+      onInit: initChart
     };
   },
-  components: {},
   methods: {
 
     // async getData() {
