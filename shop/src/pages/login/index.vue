d4854e1552215f3626ec315028540daa5c39a043
zzhnokia <zzhnokia@163.com>
2019-11-15 15:11:40 +0800

    fix

--- shop/src/pages/login/index.vue
+++ shop/src/pages/login/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="login">
     <div class="weui-cells weui-cells_after-title">
+      <div class="logo">
+        <img src="../../../static/images/edit.png">
+      </div>
       <div class="weui-cell weui-cell_input weui-cell_vcode">
         <div class="weui-cell__hd">
           <div class="weui-label">手机号</div>
@@ -8,17 +11,17 @@
         <div class="weui-cell__bd">
           <input class="weui-input" v-model="phone" placeholder="请输入手机号" />
         </div>
-        <div class="weui-cell__ft">
+        <!-- <div class="weui-cell__ft">
           <div class="weui-vcode-btn" @click="getCode">获取验证码</div>
-        </div>
+        </div> -->
       </div>
 
       <div class="weui-cell weui-cell_input weui-cell_vcode">
         <div class="weui-cell__hd">
-          <div class="weui-label">验证码</div>
+          <div class="weui-label">密码</div>
         </div>
         <div class="weui-cell__bd">
-          <input class="weui-input" v-model="verifyCode" placeholder="请输入验证码" />
+          <input class="weui-input" v-model="verifyCode" placeholder="请输入密码" />
         </div>
         <!-- <div class="weui-cell__ft">
           <image class="weui-vcode-img" src="/static/images/vcode.jpg" style="width: 108px" />
